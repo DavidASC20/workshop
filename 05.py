18cd066719d07ee0c6210114d279082ffc57a8cc
David Chong <djchong242gmail.com>
2021-09-26 13:32:09 -0400

    put the actual names instead

--- 05.py
+++ 05.py
@@ -9,72 +9,16 @@
 #comments - changed pd1 and pd2 to include actual names.
 import random
 
-class1 = [
-    "cliu20",
-    "llee20",
-    "ekrechmer20",
-    "aalonso20",
-    "owang20",
-    "schoi20",
-    "ttakada20",
-    "dmaheshwari20",
-    "wchen20",
-    "tfahey20",
-    "jnelson20",
-    "mlo20",
-    "tnguyen20",
-    "agoenka20",
-    "sanand20",
-    "ebuller20",
-    "rroy20",
-    "yelsharawy20",
-    "gmcginley20",
-    "rzheng21",
-    "sethun20",
-    "imahid20",
-    "zlin20",
-    "imijacika20",
-    "hgan20",
-    "ilam21",
-    "ltomwong20",
-    "tacuna20"
-]
+class1 = ["Alejandro Alonso", "Aryaman Goenka", "Christopher Liu", "Deven Maheshwari", "Emma Buller", "Ella Krechmer", "Gavin McGinley",
+       "Haotian Gan", "Ivan Lam", "Ishraq Mahid", "Ivan Mijacika", "Julia Nelson", "Lucas Lee", "Lucas Tom wong",
+       "Michelle Lo", "Oscar Wang", "Rayat Roy", "Reng geng Zheng", "Shriya Anand", "Shyne Choi", "Sadid Ethun",
+       "Tomas Acuna", "Theodore Fahey", "Tina Nguyen", "Tami Takada", "William Chen", "Yusef Elsharawy", "Zhao yu Lin"]
 
-class2 = [
-    "alin21",
-    "naronesty20",
-    "dsooknanan20",
-    "kcao20",
-    "rxiao20",
-    "aabdullah20",
-    "ywu20",
-    "ichang20",
-    "wdong20",
-    "jzou20",
-    "pging20",
-    "ryeung20",
-    "jlee25",
-    "eguo20",
-    "azhang22",
-    "qliu20",
-    "rshrestha20",
-    "angan20",
-    "dchong20",
-    "eknapp20",
-    "mborczuk20",
-    "hzhang20",
-    "tyu20",
-    "jwu20",
-    "jxie20",
-    "jkloepfer20",
-    "srakib20",
-    "lwong20",
-    "sliu202",
-    "yliangli20",
-    "cnelson20",
-    "ajuang20",
-    "hhuang20"
-]
+class2 = ["Alif Abdullah", "Andrew Juang", "Andy Lin", "Austin Ngan", "Annabel Zhang", "Cameron Nelson", "David Chong",
+        "Daniel Sooknanan", "Eric Guo", "Eliza Knapp", "Hebe Huang", "Han Zhang", "Yoonah Chang", "Joshua Kloepfer",
+        "Josephine Lee", "Jonathan Wu", "Jesse Xie", "Justin Zou", "Kevin Cao", "Liesel Wong", "Michael Borczuk",
+        "Noakai Aronesty", "Patrick Ging", "Qina Liu", "Roshani Shrestha", "Rachel Xiao", "Raymond Yeung", "Sophie Liu",
+        "Shadman Rakib", "Thomas Yu", "Wen hao Dong", "Yaying Liang li", "Yuqing Wu"]
 
 
 def random_student():
