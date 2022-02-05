615b6b33140f61eb37bd797d9479ad6e6d7d2a1e
DavidASC20 <djchong24@gmail.com>
2022-02-05 18:11:38 -0500

    committing javascript, not done yet

---
+++ k27_js/fibfac.js
@@ -0,0 +1,29 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K27 -- Basic functions in JavaScript
+// 2022-02-03r
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function factorial(n){
+    if(n == 1){
+        return 1;
+    }else{
+        return(n * factorial(n-1));
+    }
+}
+
+function fib(n){
+    if(n <= 1){
+        return n;
+    }else{
+        return (fib(n-1) + fib)
+    }
+}
