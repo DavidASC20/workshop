2396f556b5f2ff92155cbf1e4f5b9ca1d6915ec8
DavidASC20 <djchong24@gmail.com>
2022-02-08 08:38:36 -0500

    playing around w stuff

---
+++ k28_jsdom/dommy_consolery.js
@@ -0,0 +1,129 @@
+/*
+   your PPTASK:
+   
+   Test drive each bit of code in this file,
+    and insert comments galore, indicating anything
+     you discover,
+    	have questions about,
+    		or otherwise deem notable.
+    		
+    		Write with your future self or teammates in mind.
+    		
+    		If you find yourself falling out of flow mode, consult 
+    		other teams
+    		MDN
+
+   A few comments have been pre-filled for you...
+   
+   (delete this block comment once you are done)
+*/
+
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2022-02-08t
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-J in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 15,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+
+
+addItem(i);
+let xan = "i love pie";
+addItem(xan);
+removeItem(0);
+red();
+stripe();
+//insert your implementations here for...
+// FIB
+function fib(n){
+  if(n <= 1){
+      return n;
+  }else{
+      return (fib(n-1) + fib)
+  }
+}
+// FAC
+factorial = function(n){
+  if(n == 1){
+      return 1;
+  }else{
+      return(n * factorial(n-1));
+  }
+}
+// GCD
+gcd = function(a, b){
+  smallest = a;
+  if(a > b){
+      smallest = b;
+  }
+  // console.log("smallest is " + smallest);
+  for(let i = smallest; i >= 1; i--){
+      // console.log("i is " + i);
+      // console.log("a % i is " + a%i)
+      // console.log("b % i is " + b%i)
+      if(a % i == 0 && b % i == 0){
+          return i;
+      }
+  }
+}
+
+addItem("The GCD of 1280 and 146 is " + gcd(1280,146));
+addItem()
